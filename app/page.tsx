b7b7ad07458a61652fa50266f484737c62157e10
ido evergreen <dicksonevergreenido@gmail.com>
2024-01-02 23:45:10 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,14 @@ import SignUpUserSteps from "@/components/SignUpUserSteps";
 import Header from "@/components/Header";
 import { cookies } from "next/headers";
 import { redirect } from 'next/navigation'
+import { createClient } from "@/utils/supabase/server";
+
 export default async function Index() {
   const cookieStore = cookies();
-
+const supabase = createClient(cookies());
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
   const canInitSupabaseClient = () => {
     // This function is just for the interactive tutorial.
     // Feel free to remove it once you have Supabase connected.
